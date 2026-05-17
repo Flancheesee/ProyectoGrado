5abd19b4b77a9e69e3f087ebe24496280a9fadee
flancheesee <franci.insti@gmail.com>
2026-05-17 19:35:29 +0200

    feat: añadido web de soporte

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>MOVE IT</title>
-        <link rel="stylesheet" href="{{ asset('CSS/index.css') }}">
+        <link rel="stylesheet" href="{{ asset('CSS/support.css') }}">
     </head>
 
     <body>
@@ -14,8 +14,12 @@
         @section('contenido')
 
             <article id="infoPrincipal">
-    
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque similique rem minima, nihil dolorem inventore veritatis accusamus. Pariatur accusamus quia deserunt veritatis perspiciatis! Dolorum error dolores laboriosam, quod iusto consequatur ad libero sit porro pariatur, exercitationem aspernatur dolor. Quia dolor officia, est odit ipsum consectetur odio libero pariatur earum facilis, asperiores beatae porro quod totam labore sunt veniam modi. Tempora esse quaerat dolorum fugit soluta. Tempora corporis alias tenetur, natus dolores voluptatem dolorem voluptates quo exercitationem consequatur, delectus reiciendis doloribus! Suscipit sit repellendus magnam nostrum, deserunt, culpa nulla atque, doloremque veritatis voluptas ipsum nihil dolorum aliquam repudiandae sint pariatur quis placeat alias laudantium cum. Provident doloribus quidem assumenda ad ipsum. Dignissimos ipsum voluptas itaque similique unde temporibus perspiciatis eius, dolores aperiam possimus minima vero omnis, sit excepturi? Blanditiis eaque labore laboriosam quibusdam beatae fugit, quisquam unde, error, ducimus enim rerum nesciunt. Exercitationem error aliquam beatae ullam, vel tempora, molestiae placeat facere dolor vitae expedita aut enim optio at sed suscipit? Quam iste voluptas quo natus tempore atque aut porro, a nobis deserunt necessitatibus magni laborum culpa, commodi deleniti sed, consectetur est vitae beatae. Recusandae tempora suscipit dolore amet rem aut odit illum perferendis nemo hic temporibus esse, quisquam maiores quidem.</p>
+
+                <div id="tlfn_ayuda" class="ayuda">
+                    <p class="ayuda_text">Si necesitas ayuda no dudes en llamarnos</p>
+                    <img class="ayuda_img" src="{{ asset('IMG/ayuda_tlfn.webp') }}" alt="telefono">
+                    <p class="ayuda_info">+34 673 223 897</p>
+                </div>
             </article>
         @endsection()
     </body>
