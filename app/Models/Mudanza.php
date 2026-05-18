bb515c602276754b9f4f4f39e00b50a929d207b4
flancheesee <franci.insti@gmail.com>
2026-05-18 16:11:07 +0200

    fix: bad naming

--- app/Models/Mudanza.php
+++ app/Models/Mudanza.php
@@ -22,8 +22,6 @@ class Mudanza extends Model
         'fecha_mudanza'
     ];
 
-    dd($request->all());
-
     public function usuario() {
         return $this->belongsTo(User::class, 'mote_usuario', 'user_id');
     }
